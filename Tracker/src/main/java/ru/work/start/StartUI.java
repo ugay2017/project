2fff2cec89776cec4332b2a4ef026d37f79c4f2f
ugay2017 <ugay2016@yandex.ru>
2018-10-17 11:55:36 +0400

    tracker

--- Tracker/src/main/java/ru/work/start/StartUI.java
+++ Tracker/src/main/java/ru/work/start/StartUI.java
@@ -7,8 +7,8 @@ public class StartUI {
 	public static void main(String[] args) {
 	Tracker tracker = new Tracker();
 	tracker.add(new Task("first task", "first desc"));
-	for(Item item: tracker.getAll()) {
-		System.out.println(item.getName());
-	}
+		for(Item item: tracker.getAll()) {
+			System.out.println(item.getName());
+		}
 	}
 }
